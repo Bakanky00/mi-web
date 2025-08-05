50cf6befe0a6262b9ddb6d97d7707ed341ccfd0a
unknown <kinkimoreira9@gmail.com>
2025-08-05 12:23:43 -0300

    cambio de fondo

--- mi-pagina/src/pages/Inicio.jsx
+++ mi-pagina/src/pages/Inicio.jsx
@@ -10,7 +10,9 @@ export default function Inicio() {
         <img src={logo} alt="Logo World Of Ayrton" className="logo" />
 
         <h1>Bienvenido a World Of Ayrton</h1>
+        
         <h2>¿Qué es esta página?</h2>
+
         <Presentacion />
         <Habilidades />
         <Objetivos />
